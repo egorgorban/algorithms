db13165a3673002b669dfbcb34d23c0e6cefb96a
egorgorban <egor_gorbann@mail.ru>
2020-12-01 00:05:53 +0300

    add graph class and main.py

---
+++ Graphs/graph.py
@@ -0,0 +1,84 @@
+from random import randint as rint, uniform as rfloat
+
+
+class Graph():
+
+    def __init__(self, is_empty=True, v_n=-1, e_n=-1):
+        if not is_empty:
+            if not v_n > 0:
+                v_n = rint(5, 10)
+            if not e_n > 0:
+                e_n = rint(int(v_n*(v_n-1)/6), int(v_n*(v_n-1)/2))
+
+            _adjacent_list = [{i: 0} for i in range(v_n)]
+            for _ in range(e_n):
+                u = rint(0, v_n-1)
+                v = rint(0, v_n-1)
+                while v == u or v in _adjacent_list[u]:
+                    u = rint(0, v_n-1)
+                    v = rint(0, v_n-1)
+                (_adjacent_list[u])[v] = rint(-5, 15)  # вес ребра
+
+            self.vertices_number = v_n
+            self.edges_number = e_n
+            self.edges, self.adjacent_matrix = self._make_matrix_from_list(
+                _adjacent_list, True)
+            self.adjacent_list = _adjacent_list
+        else:
+            self.vertices_number = 0
+            self.edges_number = 0
+            self.edges = []
+            self.adjacent_list = []
+            self.adjacent_matrix = []
+
+    def _make_matrix_from_list(self, l: list, edges=False) -> list:
+        n = len(l)
+        m = [[float('inf')] * n for i in range(n)]
+        if edges:
+            e = []
+        for i in range(n):
+            dicts = l[i]
+            for d in dicts.items():
+                if edges and i != d[0]:
+                    e.append((i, d[0], d[1]))
+                m[i][d[0]] = d[1]
+        if edges:
+            return e, m
+        return m
+
+    def get_vertices_number(self):
+        return self.vertices_number
+
+    def get_edges_number(self):
+        return self.edges_number
+
+    def add_vertex(self) -> int:
+        '''
+        Добавляет вершину, 
+        Возвращает её индекс в графе
+        '''
+        self.vertices_number += 1
+        self.adjacent_list.append({self.vertices_number: 0})
+        self.adjacent_matrix = self._make_matrix_from_list(self.adjacent_list)
+        return self.vertices_number
+
+    def add_edge(self, u, v, w, is_change=False) -> bool:
+        if not is_change:
+            if v in self.adjacent_list[u]:
+                return False
+            self.edges_number += 1
+        else:
+            curr_w = self.adjacent_matrix[u][v]
+            self.edges.remove((u, v, curr_w))
+        self.edges.append((u, v, w))
+        self.adjacent_list[u][v] = w
+        self.adjacent_matrix[u][v] = w
+
+    def get_edges(self):
+        return self.edges
+
+    def get_adjacent_list(self):
+        return self.adjacent_list
+
+    def get_adjacent_matrix(self):
+        return self.adjacent_matrix
---
+++ Graphs/main.py
@@ -0,0 +1,7 @@
+from graph import Graph
+
+
+new_graph = Graph(False, 5, 10)
+print(new_graph.get_edges())
+print(new_graph.get_adjacent_list())
+print(new_graph.get_adjacent_matrix())
