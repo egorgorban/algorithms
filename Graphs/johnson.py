e9ba897f3ede8ee64b3ba05c3a0012d64ad319cb
egorgorban <egor_gorbann@mail.ru>
2020-12-07 07:19:56 +0300

    add jonhson algo

--- Graphs/johnson.py
+++ Graphs/johnson.py
@@ -1,6 +1,7 @@
 from graph import Graph
 from bellman_ford import Bellman_Ford
 from dijkstra import Dijkstra
+from copy import deepcopy
 
 
 def Johnson(G: Graph):
@@ -9,15 +10,20 @@ def Johnson(G: Graph):
     # изменяем веса на неотрицательные
     # 1) считаем значения функции h(u)
 
-    G_1 = G.copy()
+    G_1 = deepcopy(G)
     new_vertex = G_1.add_vertex()
     for v in range(n):
         G_1.add_edge(new_vertex, v, 0)
     h = Bellman_Ford(G_1, new_vertex)
+    if not h:
+        print('The graph contains negative cycle')
+        return []
+    h = h[:-1]
 
     # 2) w'(u-v) = w(u-v) + h(u) - h(v)
-    G_2 = G.copy()
-    for edge in G_2.edges:
+    G_2 = deepcopy(G)
+    edges = G_2.get_edges()
+    for edge in edges:
         G_2.add_edge(edge[0], edge[1],
                      edge[2] + h[edge[0]] - h[edge[1]], True)
 
@@ -25,5 +31,7 @@ def Johnson(G: Graph):
     shortest_paths = []
     for i in range(n):
         shortest_paths.append(Dijkstra(G_2, i))
+        for j in range(n):
+            shortest_paths[i][j] -= (h[i] - h[j])
 
     return shortest_paths
